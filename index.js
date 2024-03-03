1acaff0afc07b4643daa3876ebb672565903124b
srinanoo <srinanoo@gmail.com>
2024-03-03 21:15:05 +0530

    first committ

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ require('dotenv').config();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors({
-    origin: ['http://127.0.0.1:5173/', 'http://localhost:3000/']
-}));
+app.use(cors());
 
 const TraineeRoutes = require('./routes/traineeRoutes');
 app.use("/trainees", TraineeRoutes);
